fb755b52779d1b5b3445fc5edbbc1e1906db0e34
teshe <teshomeabebe114@gmail.com>
2025-07-10 12:48:51 +0300

    remove the logs and deploy

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ const app = express()
 
 app.get('/', (req, res) => {
   res.send('Hello, world!')
-  console.log(req, res)
 })
 
 app.post('/', (req, res) => {
   res.send('Hello from post!')
-  console.log(req, res)
 })
 
 const PORT = process.env.PORT || 3000
