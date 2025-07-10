e76ee2e5debbd0e5785b1e04d604c8c54bb83191
teshe <teshomeabebe114@gmail.com>
2025-07-10 12:58:21 +0300

    Add logging for request bodies in GET and POST routes

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const app = express()
 
 app.get('/', (req, res) => {
   res.send('Hello, world!')
+  console.log(req.body)
 })
 
 app.post('/', (req, res) => {
   res.send('Hello from post!')
+  console.log(req.body)
 })
 
 const PORT = process.env.PORT || 3000
